bb2bb6a338b1e7315ba9e20f306758829bb0a2eb
Jacob Pfeifer <jakepf00@gmail.com>
2018-01-01 13:59:16 -0600

    Completed treasure hunt

--- treasureHunt.cpp
+++ treasureHunt.cpp
@@ -1,22 +1,65 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main() {
-    int rows, columns;
+    int rows, columns, j, k, counter;
     cin >> rows >> columns;
+    j = 0;
+    k = 0;
+    counter = 0;
 
-    char map[200][200] = 'A';
+    //char map[rows][columns];
+    vector<vector<char>> map;
+    map.resize(rows, vector<char>(columns));
 
     for (int i = 0; i < rows; i++) {
-        for (int j = 0; i < columns; j++) {
-            char temp;
-            cin >> temp;
-            map[i][j] = temp;
-            cout << map[i][j];
+        for (int j = 0; j < columns; j++) {
+            cin >> map[i][j];
         }
-        cout << endl;
     }
 
-    return 0;
+
+    for (int i = 0; i < (rows * columns); i++) {
+        switch (map[j][k]) {
+            case 'N':
+                counter++;
+                j--;
+                if (j < 0) {
+                    cout << "Out" << endl;
+                    return 0;
+                }
+                break;
+            case 'S':
+                counter++;
+                j++;
+                if (j >= rows) {
+                    cout << "Out" << endl;
+                    return 0;
+                }
+                break;
+            case 'E':
+                counter++;
+                k++;
+                if (k >= columns) {
+                    cout << "Out" << endl;
+                    return 0;
+                }
+                break;
+            case 'W':
+                counter++;
+                k--;
+                if (k < 0) {
+                    cout << "Out" << endl;
+                    return 0;
+                }
+                break;
+            case 'T':
+                cout << counter << endl;
+                return 0;
+                break;
+        }
+    }
+
+    cout << "Lost" << endl;
 }
