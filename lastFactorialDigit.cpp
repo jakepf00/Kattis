e50cfdcaee9df6a47b91ec5d44482ede37b582d0
Jacob Pfeifer <jakepf00@gmail.com>
2018-11-18 17:24:52 +0000

    Completed last factorial digit

---
+++ lastFactorialDigit.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	int x;
+	cin >> x;
+	while (x--) {
+		int y;
+		cin >> y;
+		switch (y) {
+			case 1: cout << 1 << endl; break;
+			case 2: cout << 2 << endl; break;
+			case 3: cout << 6 << endl; break;
+			case 4: cout << 4 << endl; break;
+			default: cout << '0' << endl; break;
+		}
+	}
+}
