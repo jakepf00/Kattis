75f7974ea766065bbadb57ac5280060c6802dd33
Jacob Pfeifer <jakepf00@gmail.com>
2022-01-29 08:25:55 -0600

    Completed janitor troubles

---
+++ cpp/janitortroubles.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    double a, b, c, d;
+    cin >> a >> b >> c >> d;
+    double s = (a + b + c + d) / 2;
+    cout << fixed << setprecision(8) << sqrt((s-a) * (s-b) * (s-c) * (s-d)) << endl;
+}
