bf5af3b04907a321e27a3b38844c8e95874d7520
Jacob Pfeifer <jakepf00@gmail.com>
2022-02-06 08:47:52 -0600

    Completed grading

---
+++ cpp/grading.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a, b, c, d, e, g;
+    cin >> a >> b >> c >> d >> e >> g;
+    if (g >= a) cout << "A" << endl;
+    else if (g >= b) cout << "B" << endl;
+    else if (g >= c) cout << "C" << endl;
+    else if (g >= d) cout << "D" << endl;
+    else if (g >= e) cout << "E" << endl;
+    else cout << "F" << endl;
+}
