06ccc78ddb80c16802f537282abca4da7f3ecbdd
Jacob Pfeifer <jakepf00@gmail.com>
2022-11-02 17:11:47 -0500

    Completed FYI

---
+++ cpp/fyi.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    char a, b, c;
+    cin >> a >> b >> c;
+    if (a == '5' && b == '5' && c == '5') cout << 1 << endl;
+    else cout << 0 << endl;
+}
