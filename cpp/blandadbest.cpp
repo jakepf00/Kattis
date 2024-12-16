0aadc0c3166cc652235def027eee2628f12fe874
Jacob Pfeifer <jakepf00@gmail.com>
2024-12-16 11:31:07 -0700

    Completed Blandad Best

---
+++ cpp/blandadbest.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a;
+    if (a == 1) {
+        string b;
+        cin >> b;
+        cout << b << endl;
+    }
+    else cout << "blandad best" << endl;
+}
