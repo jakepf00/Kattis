9a1e516100abf1399f8f59c5013d0a5f4a5a16ea
Jacob Pfeifer <jakepf00@gmail.com>
2024-12-22 07:29:32 -0700

    Completed Heysata

---
+++ cpp/heysata.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    char a;
+    string b;
+    cin >> n >> a >> b;
+    if (b.find(a) != string::npos) cout << "Unnar fann hana!" << endl;
+    else cout << "Unnar fann hana ekki!" << endl;
+}
