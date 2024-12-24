06c5a415b793d6cc9d0be558d9364a4b2350bef4
Jacob Pfeifer <jakepf00@gmail.com>
2024-12-24 06:37:59 -0700

    Completed Skak

---
+++ cpp/skak.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    if (a == c) cout << 1 << endl;
+    else if (b == d) cout << 1 << endl;
+    else cout << 2 << endl;
+}
