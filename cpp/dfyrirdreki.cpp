fb90f4cba0e5cce613c6f073ec726676ba4c9d68
Jacob Pfeifer <jakepf00@gmail.com>
2025-01-17 06:32:54 -0700

    Completed D Fyrir Dreki

---
+++ cpp/dfyrirdreki.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int a, b, c;
+	cin >> a >> b >> c;
+	int d = b * b - (4 * a * c);
+	if (d > 0) cout << 2 << endl;
+	else if (d < 0) cout << 0 << endl;
+	else cout << 1 << endl;
+}
