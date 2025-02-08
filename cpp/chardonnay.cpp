6fbbdf0d6d53a11b746c770fae5d4da0359ec12d
Jacob Pfeifer <jakepf00@gmail.com>
2025-02-08 06:48:36 -0700

    Completed Chardonnay

---
+++ cpp/chardonnay.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a;
+    if (a == 0) cout << 0 << endl;
+    else if (a == 7) cout << 7 << endl;
+    else cout << a + 1 << endl;
+}
