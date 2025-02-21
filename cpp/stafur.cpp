4d61dd29b38a5d1932ff3740868ce5a98cdc966c
Jacob Pfeifer <jakepf00@gmail.com>
2025-02-21 06:37:42 -0700

    Completed Stafur

---
+++ cpp/stafur.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    char a;
+    cin >> a;
+    if (a == 'Y') cout << "Kannski" << endl;
+    else if (a == 'A') cout << "Jebb" << endl;
+    else if (a == 'E') cout << "Jebb" << endl;
+    else if (a == 'I') cout << "Jebb" << endl;
+    else if (a == 'O') cout << "Jebb" << endl;
+    else if (a == 'U') cout << "Jebb" << endl;
+    else cout << "Neibb" << endl;
+}
