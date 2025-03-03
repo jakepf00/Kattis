8c61457b4200913d8c9f869e7bb7b24e998104a3
Jacob Pfeifer <jakepf00@gmail.com>
2025-03-03 06:23:54 -0700

    Completed Title Cost

---
+++ cpp/titlecost.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string a;
+    double b;
+    cin >> a >> b;
+    if (size(a) <= (int) b) cout << size(a) << endl;
+    else cout << fixed << setprecision(10) << b << endl;
+}
