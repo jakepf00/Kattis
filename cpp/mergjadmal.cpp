362eb42fc36b35f8f1eff77d6f13903f35d111c3
Jacob Pfeifer <jakepf00@gmail.com>
2025-03-09 18:19:37 -0600

    Completed Mergjad Mal

---
+++ cpp/mergjadmal.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+   string a;
+    getline(cin, a);
+    if (a.find("69") != string::npos || a.find("420") != string::npos) cout << "Mergjad!" << endl;
+    else cout << "Leim!" << endl;
+}
