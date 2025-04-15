91c6840b56331ce172be135c09b07fe7e4c09f65
Jacob Pfeifer <jakepf00@gmail.com>
2025-04-15 06:10:16 -0500

    Completed Gluttonous George

---
+++ cpp/goggi.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a, b;
+    string asdf;
+    cin >> a >> asdf >> b;
+    if (a == b) cout << "Goggi svangur!" << endl;
+    else if (a > b) cout << ">" << endl;
+    else cout << "<" << endl;
+}
