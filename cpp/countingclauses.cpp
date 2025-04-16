eae4aaa82acdc2f967dbc733d59eb89f1a786dbf
Jacob Pfeifer <jakepf00@gmail.com>
2025-04-16 06:31:27 -0500

    Completed Counting Clauses

---
+++ cpp/countingclauses.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int m;
+    cin >> m;
+    if (m < 8) cout << "unsatisfactory" << endl;
+    else cout << "satisfactory" << endl;
+}
