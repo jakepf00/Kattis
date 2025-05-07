e64ea1777e28950a145ebf200e6d9046830ecb09
Jacob Pfeifer <jakepf00@gmail.com>
2025-05-07 06:35:56 -0600

    Completed A Terrible Fortress

---
+++ cpp/aterriblefortress.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int a;
+	cin >> a;
+	long long b = 0;
+	while (a--) {
+		long long c;
+		cin >> c;
+		b += c;
+	}
+	cout << b << endl;
+}
